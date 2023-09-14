f55ac261494925937f4d5279accf42885008440f
Mike Koshakow <mzkosh@gmail.com>
2023-09-14 09:21:51 -0400

    refactor dir structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Cpt-Catnip/gc-urlshort
+
+go 1.20
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gophercises/urlshort"
+	"github.com/Cpt-Catnip/gc-urlshort/urlshort"
 )
 
 func main() {
